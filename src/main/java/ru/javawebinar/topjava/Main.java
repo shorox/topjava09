57dc1bb752bfe099f2264c906494823a7fa7c534
shorox <skazo4nix@gmail.com>
2016-12-13 23:35:11 +0300

    HW1

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -9,6 +9,8 @@ package ru.javawebinar.topjava;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.format("Hello Topjava Enterprise!");
+        //System.out.format("Hello Topjava Enterprise!");
+        System.getenv("TOPJAVA_ROOT");
+
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -3,19 +3,20 @@ package ru.javawebinar.topjava.web;
 import org.slf4j.Logger;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
-public class MealServlet {
+public class MealServlet extends HttpServlet{
     private static final Logger LOG = getLogger(MealServlet.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         LOG.debug("redirect to meals");
 
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
+//        request.getRequestDispatcher("/meals.jsp").forward(request, response);
         response.sendRedirect("users.jsp");
     }
 }
